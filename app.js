dd7d363ab970a2064d2594afe631ce8d41c48cc4
Victor <93828164+VICTOROZOR@users.noreply.github.com>
2024-04-29 22:28:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function getWeather() {
+  const apiKey = "95b972b465343cab24e43841683998f1";
+  const city = document.getElementById("city").value;
+
+  if (!city) {
+    alert("Please enter a city");
+    return;
+  }
+}
